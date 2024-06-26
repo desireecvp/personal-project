371957acdedcda9ca2283262b2a5e3f465d5c092
desiree <desireecamillevieira@gmail.com>
2024-06-26 11:31:52 +0200

    my portfolio

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return(
+    <header>
+      <h1>My Portfolio</h1>
+      <p>Desiree Possiedi</p>
+      <p>Front-End Web Developer</p>
+    </header>
+    
+
+  ) 
+}
+
